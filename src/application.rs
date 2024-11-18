45d4d5b35a2239a883bf2b3de5195802692e445b
Daniel Garcia Moreno <daniel.garcia@suse.com>
2024-11-18 15:15:58 +0100

    Remove from autostart if exists in /etc/xdg

--- src/application.rs
+++ src/application.rs
@@ -3,6 +3,54 @@ use gtk::{gio, glib};
 
 use crate::{config, widgets::Window};
 
+fn copy_desktop_file(file: gio::File) {
+    // Copy .desktop file to user-config-dir and set Hidden=true property
+    let mut home_path = glib::user_config_dir().clone();
+    let file_name = String::from("autostart/") + config::APP_ID + ".desktop";
+    home_path.push(&file_name);
+    let dst = gio::File::for_path(&home_path);
+
+    // Copy the source file to user config
+    if let Err(e) = file.copy(
+        &dst,
+        gio::FileCopyFlags::OVERWRITE,
+        gio::Cancellable::NONE,
+        None,
+    ) {
+        log::error!(
+            "Error copying {:?} to {:?}: {}",
+            &file.path(),
+            &home_path,
+            e
+        );
+    }
+
+    // Append "Hidden=true" to the end of the file to disable for this user
+    // https://specifications.freedesktop.org/autostart-spec/latest/#id-1.3.4.3
+    match dst.append_to(gio::FileCreateFlags::NONE, gio::Cancellable::NONE) {
+        Ok(stream) => {
+            if let Err(e) = stream.write("Hidden=true\n".as_bytes(), gio::Cancellable::NONE) {
+                log::error!("Error removing from autostart: {}", e);
+            }
+        }
+        Err(e) => log::error!("Error removing from autostart: {}", e),
+    }
+}
+
+fn remove_from_autostart() {
+    // Remove from autostart by default if the desktop file is in autostart
+    for d in glib::system_config_dirs() {
+        let mut path = d.clone();
+        let file_name = String::from("autostart/") + config::APP_ID + ".desktop";
+        path.push(&file_name);
+        let file = gio::File::for_path(&path);
+        if file.query_exists(gio::Cancellable::NONE) {
+            log::info!("Remove from xdg autostart {:?})", &path);
+            copy_desktop_file(file);
+        }
+    }
+}
+
 mod imp {
     use std::cell::OnceCell;
 
@@ -32,6 +80,11 @@ mod imp {
             application.add_window(&window);
             window.present();
             self.window.set(window.downgrade()).unwrap();
+        }
+
+        fn shutdown(&self) {
+            remove_from_autostart();
+            self.parent_shutdown();
         }
 
         fn startup(&self) {
