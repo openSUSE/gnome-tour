669c499a78f510db1804dcc6a40e94249e4f4c5b
Daniel Garcia Moreno <daniel.garcia@suse.com>
2025-10-16 08:06:18 +0200

    opensuse: Use custom gettext domain
    Use a custom gettext domain for opensuse-welcome bin.

--- src/minimal.rs
+++ src/minimal.rs
@@ -12,17 +12,17 @@ use config::{GETTEXT_PACKAGE, LOCALEDIR};
 fn main() -> glib::ExitCode {
     let mut log_builder = env_logger::builder();
     // Compatibility G_MESSAGES_DEBUG env var
-    if !glib::log_writer_default_would_drop(glib::LogLevel::Debug, Some("gnome_tour")) {
-        log_builder.filter_module("gnome_tour", log::LevelFilter::Debug);
+    if !glib::log_writer_default_would_drop(glib::LogLevel::Debug, Some("opensuse_welcome")) {
+        log_builder.filter_module("opensuse_welcome", log::LevelFilter::Debug);
     }
     log_builder.init();
 
     // Prepare i18n
     setlocale(LocaleCategory::LcAll, "");
-    bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR)
-        .unwrap_or_else(|_| panic!("Unable to bind text domain for {GETTEXT_PACKAGE}"));
-    textdomain(GETTEXT_PACKAGE)
-        .unwrap_or_else(|_| panic!("Unable to switch to text domain {GETTEXT_PACKAGE}"));
+    bindtextdomain("opensuse-welcome", LOCALEDIR)
+        .unwrap_or_else(|_| panic!("Unable to bind text domain for opensuse-welcome"));
+    textdomain("opensuse-welcome")
+        .unwrap_or_else(|_| panic!("Unable to switch to text domain opensuse-welcome"));
 
     glib::set_application_name(&gettext("Tour"));
 
